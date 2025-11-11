00071b07585f34d919a1dd1651eba742daca6722
Nerea Puertas <nereapp69@educastur.es>
2025-11-11 10:55:28 +0100

    react

---
+++ UNIDAD5/hola-mundo/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import AcercaDe from './components/AcercaDe'
+import './App.css'
+
+function App() {
+  return <AcercaDe/>
+ 
+}
+
+export default App
---
+++ UNIDAD5/hola-mundo/src/components/AcercaDe.jsx
@@ -0,0 +1,14 @@
+const AcercaDe = () => {
+    return(
+        <>
+            <h1>Acerca de</h1>
+            <p>
+                Hola.
+                <br/>
+                ¿Como vas?
+            </p>
+        </>
+    )
+
+}
+    export default AcercaDe;
